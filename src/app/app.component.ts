ad32767924f72c6504bb2e9d08c271507c5c2b8b
Alvaro Ramirez Castillo <alvaro@MacBook-Pro-de-Alvaro.local>
2024-09-17 14:39:36 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,40 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'reader-file';
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+
+      // Verifica que sea un archivo .txt
+      if (file.type === 'text/plain') {
+        const reader = new FileReader();
+
+        // Maneja el contenido una vez leído
+        reader.onload = (e: any) => {
+          const fileContent = e.target.result;
+
+
+          console.log(1, fileContent);
+
+          // Valida el contenido del archivo aquí
+          // Por ejemplo, verifica si el archivo contiene una palabra específica
+          if (fileContent.includes('palabra clave')) {
+            console.log('El archivo es válido.');
+          } else {
+            console.log('El archivo no es válido.');
+          }
+        };
+
+        // Lee el archivo como texto
+        reader.readAsText(file);
+      } else {
+        console.error('Por favor, selecciona un archivo .txt válido.');
+      }
+    }
+  }
+
+
 }
